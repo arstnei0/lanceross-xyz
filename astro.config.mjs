9a1be8b2d08def385213e196ef5e25fe03f2e517
102563271+lancerossdev@users.noreply.github.com <102563271+lancerossdev@users.noreply.github.com>
2022-09-08 15:44:38 +0800

    new user interface

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: 'https://lanceross.xyz',
   base: '/',
-})
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
+});
---
+++ public/script.js
@@ -0,0 +1,21 @@
+document.getElementById("tooltip").innerHTML = "Click to copy";
+
+// Click to copy email
+
+function getEmail() {
+    let email = "zyx.ssorecnal@ecnal";
+
+    let tempInput = document.createElement("INPUT");
+    document.body.appendChild(tempInput);
+    tempInput.setAttribute("value", email.split("").reverse().join(""));
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+
+    var tooltip = document.getElementById("tooltip");
+    document.getElementById("tooltip").innerHTML = "Copied!";
+    setTimeout(() => {
+        document.getElementById("tooltip").innerHTML = "Click to copy";
+    }, 2000);
+}

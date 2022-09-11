bf0ea17c246b7b7474ae198a8af57b42a1b74cbe
102563271+lancerossdev@users.noreply.github.com <102563271+lancerossdev@users.noreply.github.com>
2022-09-11 08:37:00 +0800

    removed partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,6 @@
 import { defineConfig } from 'astro/config';
-import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: 'https://lanceross.xyz',
   base: '/',
-  integrations: [
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
 });

71328c6bc544131be1fa3b0318f525c6b2657721
102563271+lancerossdev@users.noreply.github.com <102563271+lancerossdev@users.noreply.github.com>
2022-09-30 21:08:07 +0800

    used local font instead of google fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,11 +14,50 @@ module.exports = {
 				'1': 'repeat(auto-fit, minmax(24ch, 1fr));'
 			},
 			screens: {
-				'dark': {'raw': '(prefers-color-scheme: dark)'},
+				'dark': { 'raw': '(prefers-color-scheme: dark)' },
+			},
+			keyframes: {
+				"move-bg": {
+					to: {
+						backgroundPosition: "400% 0",
+					},
+				},
+			},
+			animation: {
+				"move-bg": "move-bg 8s infinite linear",
+			},
+			typography: {
+				DEFAULT: {
+					css: {
+						'code::before': {
+							content: '""'
+						},
+						'code::after': {
+							content: '""'
+						},
+						p: {
+							code: {
+								backgroundColor: '#282A36',
+								padding: '0.250rem 0.4rem',
+								borderRadius: '0.250rem',
+								fontWeight: '400',
+							}
+						},
+						li: {
+							code: {
+								backgroundColor: '#282A36',
+								padding: '0.250rem 0.4rem',
+								borderRadius: '0.250rem',
+								fontWeight: '400',
+							},
+						},
+					},
+				},
 			},
 		},
-	},
-	plugins: [
-		require('tailwind-scrollbar'),
-	],
+		plugins: [
+			require('tailwind-scrollbar'),
+			require("@tailwindcss/typography")
+		],
+	}
 }
